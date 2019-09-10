ed5cd112d3b332ef8e2eb5ad39bced8faa4067a4
Penny Tague <pmtague9@gmail.com>
2019-09-10 16:40:45 -0700

    MVP plus stretch endpoints

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
 		});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateRecordId, (req, res) => {
 	const { id } = req.params;
 
 	db('cars')
@@ -30,7 +30,7 @@ router.get('/:id', (req, res) => {
 		});
 })
 
-router.post('/', (req, res) => {
+router.post('/', validateRecordData, (req, res) => {
 	const carData = req.body;
 
 	db('cars')
@@ -48,4 +48,64 @@ router.post('/', (req, res) => {
 		})
 })
 
+router.put('/:id', validateRecordId, (req, res) => {
+	const changes = req.body;
+
+	db('cars')
+		.where('id', req.params.id)
+		.update(changes)
+		.then(count => {
+			res.status(200).json({ message: `Updated ${count} record` })
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: 'Could not update record' })
+		})
+})
+
+router.delete('/:id', validateRecordId, (req, res) => {
+	db('cars')
+		.where({ id: req.params.id })
+		.del()
+		.then(count => {
+			res.status(200).json({ message: `Deleted ${count} record` })
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: 'Could not delete record' })
+		})
+})
+
+function validateRecordId(req, res, next) {
+	const { id } = req.params;
+
+	db('cars')
+		.where({ id })
+		.then(car => {
+			if(car.length) {
+				req.car = car;
+				next();
+			} else {
+				res.status(404).json({ error: 'Could not find record' })
+			}
+		})
+}
+
+function validateRecordData(req, res, next) {
+	const { vin, make, model, mileage } = req.body;
+
+	if(!vin) {
+		return res.status(400).json({ error: 'VIN is a required field' })
+	}
+	if(!make) {
+		return res.status(400).json({ error: 'Make is a required field' })
+	}
+	if(!model) {
+		return res.status(400).json({ error: 'Model is a required field' })
+	}
+	if(!mileage) {
+		return res.status(400).json({ error: 'Mileage is a required field' })
+	}
+	next()
+}
 module.exports = router;
