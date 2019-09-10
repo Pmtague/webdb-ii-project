a18929e7bbd3afadd2534bae5ec4816b81d00ca4
Penny Tague <pmtague9@gmail.com>
2019-09-10 15:55:28 -0700

    Almost MVP, something janky with db files

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,7 +1,51 @@
 const express = require('express');
 
-const db = require('../db-config.js');
+const db = require('../data/db-config.js');
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+	db('cars')
+		.then(cars => {
+			res.status(200).json(cars)
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: 'Failed to retrieve cars' })
+		});
+});
+
+router.get('/:id', (req, res) => {
+	const { id } = req.params;
+
+	db('cars')
+		.where({ id })
+		.first()
+		.then(car => {
+			res.status(200).json(car)
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: 'Failed to retrieve cars' })
+		});
+})
+
+router.post('/', (req, res) => {
+	const carData = req.body;
+
+	db('cars')
+		.insert(carData)
+		.then(ids => {
+			db('cars')
+				.where({ id: ids[0] })
+				.then(newCar => {
+					res.status(201).json(newCar)
+				})
+		}) 
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({ error: 'Failed to store carData' })
+		})
+})
 
 module.exports = router;
