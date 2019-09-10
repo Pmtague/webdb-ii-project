5ddc27792453fa4ff0ba9be34b2baca09e98a18a
Penny Tague <pmtague9@gmail.com>
2019-09-10 15:14:46 -0700

    Server up, schema migration complete

---
+++ cars/cars-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const db = require('../db-config.js');
+
+const router = express.Router();
+
+module.exports = router;
---
+++ migrations/20190910134841_create-cars-table.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+	tbl.increments();
+	tbl.text('vin')
+		.unique()
+		.notNullable();
+	tbl.text('make')
+		.notNullable();
+	tbl.text('model')
+		.notNullable();
+	tbl.integer('mileage')
+		.notNullable();
+	tbl.text('transmission');
+	tbl.text('title_status');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
